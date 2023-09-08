8afaef18975463ef5d479052d2a3f6aceb61754b
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-09-08 13:03:51 +0200

    basic login and header setup

---
+++ js/backend.js
@@ -0,0 +1,69 @@
+// backend indentifier
+const STORAGE_TOKEN = "HXZH6LGQSB8KPYZZ39S7IBUKJD8VHEQUH375LYY8";
+// URL Backend
+const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
+
+/**
+ * save data on server with a key value pair
+ * @param key id to find the data
+ * @param value value to save
+ * @returns
+ */
+async function setItem(key, value) {
+  const payload = { key, value, token: STORAGE_TOKEN };
+  return fetch(STORAGE_URL, {
+    method: "POST",
+    body: JSON.stringify(payload),
+  }).then((res) => res.json());
+}
+
+/**
+ * get data from server
+ * @param key id to find the data
+ * @returns requested data
+ */
+async function getItem(key) {
+  const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+  return fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.data) {
+        return res.data.value;
+      }
+      throw `Could not find data with key "${key}".`;
+    });
+}
+
+/**
+ * logic for w3 include
+ */
+(function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  //Loop through a collection of all HTML elements:
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    // search for elements with a certain atrribute:
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      // Make an HTTP request using the attribute value as the file name:
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          // Remove the attribute, and call this function once more:
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+})();
--- js/script.js
+++ js/script.js
@@ -2,7 +2,20 @@
  * initial load of index html
  */
 function indexLoad() {
-  setTimeout(() => {
-    document.getElementById("index-content").innerHTML = "JOIN";
-  }, 1000);
+  // setTimeout(() => {
+  //   document.getElementById(
+  //     "index-content"
+  //   ).innerHTML = `<div w3-include-html="./login.html"></div>`;
+  // }, 1000);
+}
+
+/**
+ * show and hide the header menu
+ */
+function toggleAvatarMenu() {
+  if (document.getElementById("avatar-menu").classList.contains("d-none")) {
+    document.getElementById("avatar-menu").classList.remove("d-none");
+  } else {
+    document.getElementById("avatar-menu").classList.add("d-none");
+  }
 }
